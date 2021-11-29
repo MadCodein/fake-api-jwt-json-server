c564ac3f19f2a7d9d081ae7719a3b15c281264d7
Emmanuel Ashitey <eaashitey39@gmail.com>
2021-11-29 08:20:45 +0000

    login error handling with try catch

--- server.js
+++ server.js
@@ -95,18 +95,23 @@ server.post('/auth/register', (req, res) => {
 
 // Login to one of the users from ./users.json
 server.post('/auth/login', (req, res) => {
-  console.log('login endpoint called; request body:')
-  console.log(req.body)
-  const { email, password } = req.body
-  if (isAuthenticated({ email, password }) === false) {
-    const status = 401
-    const message = 'Incorrect email or password'
-    res.status(status).json({ status, message })
-    return
+  try {
+    console.log('login endpoint called; request body:')
+    console.log(req.body)
+    const { email, password } = req.body
+    if (isAuthenticated({ email, password }) === false) {
+      const status = 401
+      const message = 'Incorrect email or password'
+      res.status(status).json({ status, message })
+      return
+    }
+    const access_token = createToken({ email, password })
+    console.log('Access Token:' + access_token)
+    return res.status(200).json({ access_token })
+  } catch (error) {
+    console.log(error)
+    return error
   }
-  const access_token = createToken({ email, password })
-  console.log('Access Token:' + access_token)
-  res.status(200).json({ access_token })
 })
 
 server.get('/', (req, res) => {
